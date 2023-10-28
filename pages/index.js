ba3b82721da15078ec559525a7aa4129be39b677
NelliWojo <nelli.timorshina@wojo.com>
2023-10-28 17:48:13 +0200

    :sparkles: incremental static generation w/ light data

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,30 @@ import fs from "fs/promises";
 import path from "path";
 
 export async function getStaticProps() {
+  console.log("");
   // cwd : current working directory / here: we want to starmt in our dir 'next-udemy-2'
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+
   return {
     props: {
       products: data.products,
     },
+    // only matters in production
+    revalidate: 10,
   };
 }
 
